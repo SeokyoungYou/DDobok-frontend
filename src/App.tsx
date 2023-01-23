ecb2a5c978bc8ecd778f7f81936bc01298d282d2
Quartz <skyu.dev@gmail.com>
2023-01-23 16:09:31 +0900

    chore: auto-deploy test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const App: React.FC = () => {
-  return <div className="App">ddobok</div>;
+  return (
+    <div className="App">
+      <span>DDobok</span>
+      <span>Auto-deploy test</span>
+    </div>
+  );
 };
 
 export default App;
