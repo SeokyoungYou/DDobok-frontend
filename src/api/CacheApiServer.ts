1bd025b83ca2f7e0b997c3eb7705791cdc0ddeee
Quartz <skyu.dev@gmail.com>
2023-01-29 14:00:21 +0900

    merge: implement expiring caching

--- src/api/CacheApiServer.ts
+++ src/api/CacheApiServer.ts
@@ -1,6 +1,8 @@
 import { CACHE_STORAGE } from "../constants/api-constants";
 
 const BASE_URL = "https://ddobok.onrender.com/api/v1";
+const HEADER_FETCH_DATE = "fetch-date";
+const ONE_DAY_MILISECOND = 60 * 60 * 24 * 1000;
 export class CacheApiServer {
   private static giCacheStorage = CACHE_STORAGE.GIS;
   private static brandsCacheStorage = CACHE_STORAGE.BRANDS;
@@ -8,27 +10,53 @@ export class CacheApiServer {
   static async getGisByQuery(query: string) {
     const url = `${BASE_URL}/gis/${query}`;
     const cache = await caches.open(this.giCacheStorage);
-    const cacheResponse = await caches.match(url);
 
-    return cacheResponse
-      ? await cacheResponse.json()
-      : await this.getFetchResponse(cache, url);
+    return await this.getValidResponse(cache, url);
   }
 
   static async getBrandById(id: number) {
     const url = `${BASE_URL}/brands/${id}`;
     const cache = await caches.open(this.brandsCacheStorage);
+
+    return await this.getValidResponse(cache, url);
+  }
+
+  private static async getValidResponse(cache: Cache, url: string) {
     const cacheResponse = await caches.match(url);
 
-    return cacheResponse
-      ? cacheResponse.json()
-      : this.getFetchResponse(cache, url);
+    return cacheResponse && !this.isCacheExpired(cacheResponse)
+      ? await cacheResponse!.json()
+      : await this.getFetchResponse(cache, url);
+  }
+
+  private static isCacheExpired(cacheResponse: Response) {
+    const fetchDate = new Date(
+      cacheResponse.headers.get(HEADER_FETCH_DATE)!
+    ).getTime();
+    const today = new Date().getTime();
+
+    return today - fetchDate > ONE_DAY_MILISECOND;
   }
 
   private static async getFetchResponse(cache: Cache, url: string) {
     const fetchResponse = await fetch(url);
-    cache.put(url, fetchResponse.clone());
+    const newResponse = await this.getResponseWithFetchDate(fetchResponse);
+    cache.put(url, newResponse);
+
     console.info("fetching api");
     return fetchResponse.json();
+  }
+
+  private static async getResponseWithFetchDate(fetchResponse: Response) {
+    const cloneResponse = fetchResponse.clone();
+    const newBody = await cloneResponse.blob();
+    let newHeaders = new Headers(cloneResponse.headers);
+    newHeaders.append(HEADER_FETCH_DATE, new Date().toISOString());
+
+    return new Response(newBody, {
+      status: cloneResponse.status,
+      statusText: cloneResponse.statusText,
+      headers: newHeaders,
+    });
   }
 }
